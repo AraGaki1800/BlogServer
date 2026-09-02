5f37a4e74673f067b358b20bcdb2fb9788a982e8
haocheng.tan <haocheng.tan@jiamiantech.com>
2026-09-02 18:31:46 +0800

    feat: adjust discovery code

--- article-service/src/main/java/me/thaoc/article/ArticleApplication.java
+++ article-service/src/main/java/me/thaoc/article/ArticleApplication.java
@@ -2,9 +2,7 @@ package me.thaoc.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class ArticleApplication {
     public static void main(String[] args) {
--- user-service/src/main/java/me/thaoc/user/config/AppConfig.java
+++ user-service/src/main/java/me/thaoc/user/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "blog")
+@ConfigurationProperties(prefix = "app")
 @RefreshScope
 @Getter
 @Setter
